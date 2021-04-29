7833e0712852a07e7fb36b2091ad27cf19244266
csgrabt <gy.cseko@outlook.com>
2021-04-29 10:56:26 +0200

    Phonebook feladat megoldása

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -0,0 +1,44 @@
+package hu.nive.ujratervezes.kepesitovizsga.phonebook;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Phonebook {
+
+    public static final String KEY_VALUE_SEPARATOR = ": ";
+
+    public void exportPhonebook(Map<String, String> contacts, String outputPath) {
+        validator(contacts, outputPath);
+
+
+        Path pathOfFile = Path.of(outputPath);
+
+        try (BufferedWriter br = Files.newBufferedWriter(pathOfFile, StandardCharsets.UTF_8)) {
+            writingFile(contacts, br);
+
+
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Something went wrong while writing the file!", ioe);
+
+        }
+
+    }
+
+    private void writingFile(Map<String, String> contacts, BufferedWriter br) throws IOException {
+        List<String> keys = new ArrayList<>(contacts.keySet());
+        for (String item : keys
+        ) {
+            br.write(item + KEY_VALUE_SEPARATOR + contacts.get(item) + "\n");
+        }
+    }
+
+    private void validator(Map<String, String> contacts, String outputPath) {
+        if (contacts == null || outputPath == null) {
+            throw new IllegalArgumentException("Contacts or (and) outputPath is null!");
+        }
+    }
+}
